67db71c410c280e0782a6c21bc847b29d68b99bb
Dardem30 <r.lukashenko@mail.ru>
2017-05-30 08:10:54 +0200

    Update Cat.java

--- src/com/company/Cat.java
+++ src/com/company/Cat.java
@@ -1,4 +1,16 @@
 public class Cat{
 String name;
 int age;
+  public void setName(String name){
+this.name=name;
+  }
+  public String getNAme(){
+    return name;
+  }
+  public void setAge(int age){
+    this.age=age;
+  }
+  public int getAge(){
+return age;
+  }
 }
